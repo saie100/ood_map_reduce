6ea2461e3efec0951ec5cbcf23314ae57b408830
Varis Nijat <vnijat93@gmail.com>
2023-04-10 20:42:58 -0400

    DEV Add reduce class

---
+++ src/Reduce.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Reduce.cpp
+ * @author: Varis Nijat
+ * CSE 687 Object Oriented Design in C++
+ * Syracuse University
+ * Project MapReduce
+**/
+
+#include "Reduce.h"
+
+#include <string>
+#include <list>
+
+using std::string;
+using std::list;
+
+/**
+ * Default Constructor
+*/
+Reduce::Reduce(string outputDir){};
+
+void Reduce::redue(string key, list<int> intIterator){}
---
+++ src/Reduce.h
@@ -0,0 +1,30 @@
+/**
+ * @file Reduce.h
+ * @author: Varis Nijat
+ * CSE 687 Object Oriented Design in C++
+ * Syracuse University
+ * Project MapReduce
+**/
+
+#ifndef REDUCE_H
+#define REDUCE_H
+
+#include <string>
+#include <list>
+
+using std::string;
+using std::list;
+
+class Reduce{
+
+    public:
+
+        /**
+         * Class Constructor 
+        */
+        Reduce(string outputDir);
+
+        void redue(string key, list<int> intIterator);
+};
+
+#endif
