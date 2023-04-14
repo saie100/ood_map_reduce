2657c49fc0ccd44a56d15eb4217fafbb9c4bcb5c
Varis Nijat <vnijat93@gmail.com>
2023-04-13 20:30:05 -0400

    TST Formatter

--- src/Reduce.cpp
+++ src/Reduce.cpp
@@ -5,32 +5,29 @@
  * Syracuse University
  * Project MapReduce
  * Copyright [2023] <Copyright Varis Nijat>
-**/
+ **/
 
 #include "Reduce.h"
 
-#include <string>
 #include <list>
+#include <string>
 
-using std::string;
 using std::list;
+using std::string;
 
 /**
  * Class Constructor specifying output directory
-*/
-Reduce::Reduce(string outputDir) {
-    output_dir = outputDir;
-}
+ */
+Reduce::Reduce(string outputDir) { output_dir = outputDir; }
 
 void Reduce::redue(string& key, list<int> intIterator) {
-    // Sum all the values in the list
-    int sum = 0;
-    for (int i  : intIterator) {
-        sum += i;
-    }
+  // Sum all the values in the list
+  int sum = 0;
+  for (int i : intIterator) {
+    sum += i;
+  }
 
-    export_result(key, sum);
+  export_result(key, sum);
 }
-
 
 void Reduce::export_result(string& key, int value) {}
