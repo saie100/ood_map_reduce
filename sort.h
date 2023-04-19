1c0e4f2028264f3e02a3d7fe30134c444a2775ec
karim2629 <69849468+karim2629@users.noreply.github.com>
2023-04-18 17:37:14 -0700

    Add files via upload

---
+++ sort.h
@@ -0,0 +1,15 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <string>
+#include <vector>
+
+class Sort {
+private:
+    std::string extractString(const std::string& pairString);
+
+public:
+    void Reduce();
+};
+
+#endif
