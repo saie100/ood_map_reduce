fdc127a3c10eb5cfb086725c07e9d25d859bb3ce
Varis Nijat <vnijat93@gmail.com>
2023-04-23 23:20:58 -0400

    STY Clang-format

--- src/Workflow.cpp
+++ src/Workflow.cpp
@@ -1,46 +1,43 @@
 #include "headers/Workflow.h"
-#include "headers/FileManager.h"
-#include "headers/Map.hpp"
-#include "headers/Sort.h"
-#include "headers/Reduce.h"
 
 #include <string>
 #include <vector>
 
-using std::string;
+#include "headers/FileManager.h"
+#include "headers/Map.hpp"
+#include "headers/Reduce.h"
+#include "headers/Sort.h"
+
 using std::cout;
 using std::endl;
+using std::string;
 
 /**
  * Class Constructor specifying directories
  */
-Workflow::Workflow(string input_dir, string temp_dir, string output_dir):
-inputDir(input_dir),tempDir(temp_dir),outputDir(output_dir){
-    
-}
-
+Workflow::Workflow(string input_dir, string temp_dir, string output_dir)
+    : inputDir(input_dir), tempDir(temp_dir), outputDir(output_dir) {}
 
 void Workflow::start() {
-    FileManager fm = FileManager();
-    vector<string> inputFilePaths = fm.getFilesFromDir(inputDir);
+  FileManager fm = FileManager();
+  vector<string> inputFilePaths = fm.getFilesFromDir(inputDir);
 
-    string tempMapOutputFilePath = tempDir + "/tempMapOutput.txt";
-    string tempSortOutputFilePath = tempDir + "/tempSortOutput.txt";
+  string tempMapOutputFilePath = tempDir + "/tempMapOutput.txt";
+  string tempSortOutputFilePath = tempDir + "/tempSortOutput.txt";
 
-    Map m = Map(tempMapOutputFilePath);
-    Sort s;
-    Reduce r = Reduce(tempSortOutputFilePath, outputDir);
+  Map m = Map(tempMapOutputFilePath);
+  Sort s;
+  Reduce r = Reduce(tempSortOutputFilePath, outputDir);
 
-    for (string inputFilePath: inputFilePaths){
+  for (string inputFilePath : inputFilePaths) {
+    array<string, 2> inputFile = fm.readFile(inputFilePath);
+    string inputFileName = inputFile[0];
+    string inputContent = inputFile[1];
 
-        array<string, 2> inputFile = fm.readFile(inputFilePath);
-        string inputFileName = inputFile[0];
-        string inputContent = inputFile[1];
+    m.map(inputFileName, inputContent);
+  }
 
-        m.map(inputFileName, inputContent);
-    }
+  s.Reduce(tempMapOutputFilePath, tempSortOutputFilePath);
 
-    s.Reduce(tempMapOutputFilePath, tempSortOutputFilePath);
-
-    r.processSortResult();
+  r.processSortResult();
 }
