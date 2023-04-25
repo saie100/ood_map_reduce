d656305764bbefe16b167561f43b1f50ea95e5c6
karim2629 <69849468+karim2629@users.noreply.github.com>
2023-04-24 17:44:57 -0700

    update sort.cpp
    updated to take string and output string instead or reading and writing to file

--- src/headers/Sort.h
+++ src/headers/Sort.h
@@ -4,15 +4,30 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <fstream>  // For std::ofstream
 
 class Sort {
 public:
-    void Reduce(const std::string& input_filename, const std::string& output_filename);
+    // Constructor that takes the input and output file names as arguments
+    Sort(const std::string& input_filename, const std::string& output_filename);
+
+    // Public function that reduces the input file and writes the output to the output file
+    void Sorter();
 
 private:
+    std::string input_filename_;  // Input file name
+    std::string output_filename_;  // Output file name
+    std::string data;  // Input data
+    std::string result;  // Output result
+
+    // Private function that extracts the string between double quotes from a pair string
     std::string ExtractString(const std::string& pair_string);
-    bool UpdateWordCounts(const std::string& input_filename, std::map<std::string, std::vector<int>>& word_counts);
-    void SortCountsToFile(const std::map<std::string, std::vector<int>>& word_counts, std::ofstream& outfile);
+
+    // Private function that updates the word counts map based on the input file
+    bool UpdateWordCounts(std::map<std::string, std::vector<int>>& word_counts);
+
+    // Private function that sorts the word counts map and writes the output to the output file
+    void SortCountsToFile(const std::map<std::string, std::vector<int>>& word_counts);
 };
 
 #endif
