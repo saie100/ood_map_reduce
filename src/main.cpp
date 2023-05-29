8d391f5ed8694cac34cb08d0e22957425bacc82f
Varis Nijat <vnijat93@gmail.com>
2023-05-29 19:11:28 -0400

    DEV Del temp files from dir

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include "headers/Executive.h"
 #include "headers/Workflow.h"
+#include "headers/FileManager.h"
 
 using std::cin;
 using std::cout;
@@ -15,6 +16,8 @@ using std::array;
 int main(int argc, char ** argv){
 
   Executive commandline(argc, argv);
+  FileManager::deleteFilesFromDir(commandline.getTempDir());
+  FileManager::deleteFilesFromDir(commandline.getOutputDir());
   Workflow wf(
     commandline.getInputDir(),
     commandline.getTempDir(),
